a5fd26f58d8652cb0f1dda4ffc02daafe63ec04b
prasad-7 <91150388+prasad-7@users.noreply.github.com>
2023-04-18 05:18:19 +0800

    Add files via upload

---
+++ test.py
@@ -0,0 +1,50 @@
+import cv2
+import numpy as np
+from keras.models import load_model
+from googlesearch import search 
+import time
+
+model=load_model('model_file_30epochs.h5')
+
+video=cv2.VideoCapture(0)
+
+faceDetect=cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+
+labels_dict={0:'Angry',1:'Disgust', 2:'Fear', 3:'Happy',4:'Neutral',5:'Sad',6:'Surprise'}
+global label
+while True:
+    ret,frame=video.read()
+    gray=cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    faces= faceDetect.detectMultiScale(gray, 1.3, 3)
+    for x,y,w,h in faces:
+        sub_face_img=gray[y:y+h, x:x+w]
+        resized=cv2.resize(sub_face_img,(48,48))
+        normalize=resized/255.0
+        reshaped=np.reshape(normalize, (1, 48, 48, 1))
+        result=model.predict(reshaped)
+        label=np.argmax(result, axis=1)[0]
+        #print(label)
+        cv2.rectangle(frame, (x,y), (x+w, y+h), (0,0,255), 1)
+        cv2.rectangle(frame,(x,y),(x+w,y+h),(50,50,255),2)
+        cv2.rectangle(frame,(x,y-40),(x+w,y),(50,50,255),-1)
+        cv2.putText(frame, labels_dict[label], (x, y-10),cv2.FONT_HERSHEY_SIMPLEX,0.8,(255,255,255),2)
+
+        
+    cv2.imshow("Frame",frame)
+    k=cv2.waitKey(1)
+    if k==ord('q'):
+        break
+    try:
+        from googlesearch import search
+    except ImportError:
+        print("No module named 'google' found")
+
+    # to search
+    query = f" show results for {labels_dict[label]}"
+    print(labels_dict[label])
+    j = search(query, tld="co.in", num=10, stop=10, pause=2)
+    print(j.gi_yieldfrom)
+    
+    
+video.release()
+cv2.destroyAllWindows()
